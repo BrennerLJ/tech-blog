53a7c0c1ccdec3eb8019b5374cd134a82f8eecb0
BrennerLJ <bjbrenardjoey@gmail.com>
2024-06-30 22:00:25 -0600

    added to home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,80 +5,139 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
-      const dbPostData = await Post.findAll({
-        include: {model: User},
-        order: [['date_created', 'DESC']],
-      });
-      
-      const homePost = dbPostData.map((post) =>
-        post.get({ plain: true })
-      );
-
-      res.render('homepage', {
-        homePost,
-        loggedIn: req.session.loggedIn,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
-
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('login');
-  });
-  
-  //goes to post page by id
-  router.get('/post/:id', withAuth, async (req, res) => {
-    Post.findOne({
-      where: {
-          id: req.params.id
-      },
-      attributes: [
-          'id',
-          'title',
-          'content',
-          'date_created'
-      ],
-      include: [{
-              model: Comments,
-              attributes: ['id', 'content', 'post_id', 'date_created', 'user_id'],
-              include: {
-                  model: User,
-                  attributes: ['name']
-              }
+      const postData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ["name"],
           },
           {
-              model: User,
-              attributes: ['name']
-          }
-      ]
-  })
-  .then(dbPostData => {
-      if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
+            model: Comments,
+            attributes: ["comment_body"],
+          },
+        ]
+      });
+
+      const blogPosts = postData.map((blogPost) =>
+      blogPost.get({ plain: true })
+      );
+
+      res.render("homepage", {
+        blogPosts,
+        logged_in: req.session.logged_in,
+      });
+      } catch (err) {
+        res.status(500).json(err);
       }
-      const post = dbPostData.get({ plain: true });
-      console.log(post);
-      res.render('post', 
-        { 
-          post,
-          loggedIn: req.session.loggedIn 
-        });
-  
-  
-  })
-  .catch(err => {
-      console.log(err);
+});
+
+router.get("/blogPost/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {
+          model: Comments,
+          include: [User],
+        }
+      ]
+    });
+
+    const blogPost = postData.get({ plain: true });
+
+    res.render("blogPost", {
+      ...blogPost,
+      logged_in: req.session.logged_in,
+    });
+    } catch (err) {
       res.status(500).json(err);
-  });
+      res.redirect("/login");
+    }
   });
 
+  router.get("/dashboard", withAuth, async (req, res) => {
+    try {
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ["password"] },
+        include: [
+          {
+            model: Post,
+            include: [User],
+          },
+            {
+              model: Comments,
+            },
+          ]
+        });
+
+        const user = userData.get({ plain: true });
+        res.render("dashboard", {
+          ...user,
+          logged_in: true,
+        });
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      });
+      
+      router.get("/create", async (req, res) => {
+        try {
+          if (req.session.logged_in) {
+            res.render("create", {
+              logged_in: req.session.logged_in,
+              userId: req.session.user_id,
+            });
+            return;
+          } else {
+            res.redirect("/login");
+          }
+          } catch (err) {
+            res.status(500).json(err);
+          }
+        });
+
+        router.get("/create/:id", async (req, res) => {
+          try {
+            const postData = await Post.findByPk(req.params.id, {
+              include: [
+                {
+                  model: User,
+                  attributes: ["name"],
+                },
+                {
+                  model: Comments,
+                  include: [User],
+                },
+              ]
+            });
+
+            const blogPost = postData.get({ plain: true });
+
+            if (req.session.logged_in) {
+              res.render("edit", {
+                ...blogPost,
+                logged_in: req.session.logged_in,
+                userId: req.session.user_id,
+              });
+              return;
+              } else {
+                res.redirect("/login");
+              }
+              } catch (err) {
+                res.status(500).json(err);
+              }
+        });
+
+        router.all("/login", (req, res) => {
+          if (req.session.logged_in) {
+            res.redirect("/dashboard");
+            return;
+          }
+
+          res.render("login");
+        });
 
 module.exports = router;
